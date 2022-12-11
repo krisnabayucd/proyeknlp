a73729f57c2354f7daafe45f186f572454656f87
na <71653553+krisnabayucd@users.noreply.github.com>
2022-12-11 20:00:20 +0700

    Add files via upload

---
+++ webapp/1_🏠_main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Analaisis Sentimen Ulasan Film",
+    page_icon = "🏠",
+)
+
+st.title('halaman utama')
+st.sidebar.success("pilih halaman")
