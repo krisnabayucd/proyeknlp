82dbf31878a02b19beae438e87c18974dac373b1
na <71653553+krisnabayucd@users.noreply.github.com>
2022-12-11 23:14:19 +0700

    Add files via upload

--- webapp/1_🏠_main.py
+++ webapp/1_🏠_main.py
@@ -5,5 +5,12 @@ st.set_page_config(
     page_icon = "🏠",
 )
 
-st.title('halaman utama')
+st.title('Analisis Sentimen Film Marvel')
 st.sidebar.success("pilih halaman")
+
+from PIL import Image
+image = Image.open('marvel.jpg')
+
+st.image(image, caption='~')
+
+st.markdown('Aplikasi ini ditujukan untuk analisis sentimen film MCU. Model yang digunakan adalah LSTM yang telah dilatih dengan 50000 data ulasan film. Klik sidebar di samping untuk menampilkan fitur-fitur dari aplikasi')
