ae79411455232a64bb736f7b42bfcdb08b273a84
na <71653553+krisnabayucd@users.noreply.github.com>
2022-12-21 12:01:57 +0700

    Add files via upload

--- webapp/1_🏠_main.py
+++ webapp/1_🏠_main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "Analaisis Sentimen Ulasan Film",
+    page_title = "Analaisis Sentimen",
     page_icon = "🏠",
 )
 
-st.title('Analisis Sentimen Film Marvel')
+st.markdown("<h1 style='color: #22A7EC;'>Analisis Sentimen Film Marvel (MCU)</h1>", unsafe_allow_html=True)
 st.sidebar.success("pilih halaman")
 
 from PIL import Image
@@ -13,4 +13,4 @@ image = Image.open('marvel.jpg')
 
 st.image(image, caption='~')
 
-st.markdown('Aplikasi ini ditujukan untuk analisis sentimen film MCU. Model yang digunakan adalah LSTM yang telah dilatih dengan 50000 data ulasan film. Klik sidebar di samping untuk menampilkan fitur-fitur dari aplikasi')
+st.markdown('Aplikasi ini ditujukan untuk analisis sentimen film MCU berdasarkan tweets dan ulasan dari IMDB. Model yang digunakan adalah LSTM yang telah dilatih dengan 50000 data ulasan film yang bersumber dari IMDB. Klik sidebar di samping untuk menampilkan fitur-fitur dari aplikasi')
